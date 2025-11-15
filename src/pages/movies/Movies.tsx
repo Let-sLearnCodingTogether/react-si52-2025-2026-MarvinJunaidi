65f35f439992dc1b2b1cbc75c8fe59414a805654
Marvin <marvinjunaidi_2327240003@mhs.mdp.ac.id>
2025-11-15 18:38:31 +0700

    update

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,10 +1,39 @@
 import { NavLink } from "react-router"
+import { useCallback, useEffect, useState } from "react"
+import ApiClient from "../../utils/ApiClient"
+
+interface Movie {
+    _id : string,
+    judul : string,
+    tahunRilis : string,
+    sutradara : string,
+    createdAt : string,
+    updateAt : string
+}
 
 function Movies () {
-    return <div>
-        <h2>Movie Pages</h2>
-        <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    const fetchMovies = useCallback(async () => {
+        const response = await ApiClient.get("/movie")
+
+        if(response.status == 200){
+            setMovies(response.data.data)
+        }
+
+    }, [])
+
+    useEffect(() => {
+        fetchMovies()
+    }, [fetchMovies])
+
+    return <div className="container mx-auto">
+        <div className="d-flex justify-content-between mb-3">
+            <h4>Movie Pages</h4>
+            <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+        </div>
     </div>
+
 }
 
 export default Movies
